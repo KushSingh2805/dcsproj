bacb20c8a9e3eeb9f1dda10649a70736bd00d391
Kushagra <aryan1020ks@hotmail.com>
2026-09-22 23:12:38 +0530

    Generated a QAM map for 64 QAM for a user input PAM amp levels

--- modulation/convQAM.m
+++ modulation/convQAM.m
@@ -1,5 +1,22 @@
-M=input ("Enter the order of QAM (M): ");
+M=64;
 Bits_per_symbol = log2(M); % Number of bits per symbol;
 PAM=sqrt(M); % Number of PAM levels
-s=I+j*Q; % Complex symbols
-Amp_levels=input("Enter the amplitude levels for the PAM signals (as a vector): ");
+Amp_levels=[-7 -5 -3 -1 1 3 5 7];
+complex = zeros(1,64);
+c=1;
+for I=1:8
+    for Q=1:8
+        s=[Amp_levels(I)+1i*Amp_levels(Q)]
+        complex(c)=s;
+        c=c+1;
+    end
+end
+Es=2*(64-1)/3;
+for i=1:64
+    complex(i)=complex(i)/sqrt(Es);
+end
+scatter(real(complex),imag(complex),'filled');
+axis on;
+grid on;
+
+
